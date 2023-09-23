517d1fee711fc9896e876002514bd8214c00e410
Katie K <katiekucsera@gmail.com>
2023-09-23 13:06:25 -0400

    Comment out temporary testing code

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const {fetchMyIP, fetchCoordsByIP} = require('./iss.js');
 
-fetchCoordsByIP(ip, (error, coords) => {
-  if (error) {
-    console.log("Unable to get coordinates:", error);
-    return;
-  }
-
-  console.log("Success! Returned coordinates:", coords);
-
-})
-
-
 /* Test code - not currently needed
+
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("Request failed:", error);
     return;
   }
-
   console.log("Success! Returned IP:", ip);
-}); */
+}); 
+
+fetchCoordsByIP(45, (error, coords) => {
+  if (error) {
+    console.log("Unable to get coordinates:", error);
+    return;
+  }
+  console.log("Success! Returned coordinates:", coords);
+})
+*/
