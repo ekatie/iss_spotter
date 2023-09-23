fe6ae07f8d5ad04f3bf55b276aa1b740a21c0e2f
Katie K <katiekucsera@gmail.com>
2023-09-23 13:02:53 -0400

    Call fetchCoordsByIP function for testing cases

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const {fetchMyIP} = require('./iss.js');
+const {fetchMyIP, fetchCoordsByIP} = require('./iss.js');
+
+fetchCoordsByIP(ip, (error, coords) => {
+  if (error) {
+    console.log("Unable to get coordinates:", error);
+    return;
+  }
+
+  console.log("Success! Returned coordinates:", coords);
+
+})
+
 
 /* Test code - not currently needed
 fetchMyIP((error, ip) => {
