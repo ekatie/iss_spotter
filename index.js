3f2d45b5a0f4c544bccbabeee2faf88b15a76c3e
Katie K <katiekucsera@gmail.com>
2023-09-23 12:34:52 -0400

    Comment out temporary testing code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {fetchMyIP} = require('./iss.js');
 
+/* Test code - not currently needed
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("Request failed:", error);
@@ -7,4 +8,4 @@ fetchMyIP((error, ip) => {
   }
 
   console.log("Success! Returned IP:", ip);
-});
+}); */
