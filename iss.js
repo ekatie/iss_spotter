1d24fc8caf85b9e613213be9769f8940feeecc04
Katie K <katiekucsera@gmail.com>
2023-09-24 12:24:06 -0400

    Remove unnecessary function exports

--- iss.js
+++ iss.js
@@ -108,4 +108,4 @@ const nextISSTimesForMyLocation = function(callback) {
   });
 };
 
-module.exports = {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation};
+module.exports = {nextISSTimesForMyLocation};
