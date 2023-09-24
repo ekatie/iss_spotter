bc6b5d56a79eef3b0621b43be46fffe2aff7bb2d
Katie K <katiekucsera@gmail.com>
2023-09-24 15:25:20 -0400

    Call promise functions and catch errors

---
+++ index2.js
@@ -0,0 +1,18 @@
+const {nextISSTimesForMyLocation} = require('./iss_promised');
+
+const printPassTimes = function(passTimes) {
+  for (const time of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(time.risetime);
+    const duration = time.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("Unable to obtain flyover data:", error.message);
+  });
