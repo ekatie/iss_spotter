79e742321d08faf99170bcef294ce341aca2f2bf
Katie K <katiekucsera@gmail.com>
2023-09-24 13:54:38 -0400

    Implement function to format printed pass times and call it

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const {nextISSTimesForMyLocation} = require('./iss.js');
 
+const printPassTimes = function(passTimes) {
+  for (const time of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(time.risetime);
+    const duration = time.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("Unable to obtain flyover data:", error);
   }
 
-  console.log(passTimes);
+  printPassTimes(passTimes);
 });
